47e29279aadee3192f60d4c2eed8398190d91825
FrontendSuberu <joeysuberu@gmail.com>
2025-04-16 23:38:55 +0100

    addition of the logos and hero section pt-2 done / thinking of adding micro interactions after making everything responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,16 +2,52 @@ import React from "react";
 import "./home.css";
 import { Link } from "react-router-dom";
 import { motion } from "motion/react";
-Link;
+
+// sponsors
+import spon1 from "../../assets/sponsors/spon1.png";
+import spon2 from "../../assets/sponsors/spon2.png";
+import spon3 from "../../assets/sponsors/spon3.png";
+import spon4 from "../../assets/sponsors/spon4.png";
+import spon5 from "../../assets/sponsors/spon5.png";
+import spon6 from "../../assets/sponsors/spon6.png";
+import spon7 from "../../assets/sponsors/spon7.png";
+import spon8 from "../../assets/sponsors/spon8.png";
+
+const Sponsors = [
+  {
+    img: spon1,
+  },
+  {
+    img: spon2,
+  },
+  {
+    img: spon3,
+  },
+  {
+    img: spon4,
+  },
+  {
+    img: spon5,
+  },
+  {
+    img: spon6,
+  },
+  {
+    img: spon7,
+  },
+  {
+    img: spon8,
+  },
+];
 
 function Home() {
   return (
     <>
       <div className="hero">
         <motion.div
-          initial={{ opacity: 0,  y: 10}}
-          whileInView={{ opacity: 1,  y: 0}}
-          transition={{ delay: .33 }}
+          initial={{ opacity: 0, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.33 }}
           viewport={{ once: true }}
           className="hero-content"
         >
@@ -25,9 +61,24 @@ function Home() {
 
           <Link>Request a demo</Link>
         </motion.div>
+
+        {/* slider */}
+        <div className="hero_slider"></div>
       </div>
 
-      <div className="hed"></div>
+      <div className="sponsors">
+        <p>Trusted by companies of all sizes around the world</p>
+
+        <div className="sponsors_logos">
+          {Sponsors.map((sponsor, key) => {
+            return (
+              <div className="logo_wrapper">
+                <img src={sponsor.img} id={key} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
