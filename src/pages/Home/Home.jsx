9f57abc950f10324e69982b46301145ac9db3696
FrontendSuberu <joeysuberu@gmail.com>
2025-04-16 14:53:27 +0100

    installation of framer motion and body animation added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import "./home.css";
+import { Link } from "react-router-dom";
+import { motion } from "motion/react";
+Link;
 
 function Home() {
   return (
     <>
       <div className="hero">
-        <div className="hero-content">
-          <h1>Building Mentally 
-          Healthy Businesses</h1>
+        <motion.div
+          initial={{ opacity: 0,  y: 10}}
+          whileInView={{ opacity: 1,  y: 0}}
+          transition={{ delay: .33 }}
+          viewport={{ once: true }}
+          className="hero-content"
+        >
+          <h1>Building Mentally Healthy Businesses</h1>
 
-          <p>Backed by science and tailored to your business, Unmind’s comprehensive mental health platform combines therapy, coaching, AI, and expert insights to help your organization thrive.</p>
-        </div>
+          <p>
+            Backed by science and tailored to your business, Unmind’s
+            comprehensive mental health platform combines therapy, coaching, AI,
+            and expert insights to help your organization thrive.
+          </p>
+
+          <Link>Request a demo</Link>
+        </motion.div>
       </div>
+
+      <div className="hed"></div>
     </>
   );
 }
--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import Header from '../components/Header/Header'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Header from "../components/Header/Header";
+import { Outlet } from "react-router-dom";
 
 function Layout() {
-    return (
-     <>
-        <Header />
+  return (
+    <>
+      {/* component that persists header and footer in all pages */}
+      <Header />
 
+      <div>
+        <Outlet />
+      </div>
 
-        <div>
-            <Outlet />
-        </div>
-     </>   
-    )
+      <div className="cookies_btn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#0e0e0e"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+        </svg>
+      </div>
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
