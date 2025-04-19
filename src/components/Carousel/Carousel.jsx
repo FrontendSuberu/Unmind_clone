f331626c244249d2a39f3a6e7cca69adb07a6964
FrontendSuberu <joeysuberu@gmail.com>
2025-04-19 18:13:56 +0100

    downloaded images for carousel for header added

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import './Carousel.css'
+import React from "react";
+import hero1 from "../../assets/hero/hero1.png";
+import hero2 from "../../assets/hero/hero2.jpg";
+import hero3 from "../../assets/hero/hero3.png";
+import hero4 from "../../assets/hero/hero4.jpg";
+import hero5 from "../../assets/hero/hero5.jpg";
+
+import "./Carousel.css";
 
 const Carousel = () => {
-  const slides = Array.from({ length: 6 }, (_, i) => i)
+  const slides = [
+    {
+      img: hero1,
+    },
+    {
+      img: hero2,
+    },
+    {
+      img: hero3,
+    },
+    {
+      img: hero4,
+    },
+    {
+      img: hero5,
+    },
+  ];
 
   return (
     <div className="slider">
       <div className="slider-track">
-        {slides.concat(slides).map((_, i) => (
+        {slides.concat(slides).map((slide, i) => (
           <div className="slide" key={i}>
             <img
-              src="https://via.placeholder.com/300x200?text=Slide"
+              src={slide.img}
               alt={`Slide ${i + 1}`}
             />
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
